b7b8ee8e15125cec5322931726b5e3b761a67de3
Jane Sardarian <sardarianjane@gmail.com>
2023-12-21 19:31:26 +0400

    Added BookForm, BookList and Filter components

---
+++ frontend/src/components/BookForm/BookForm.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import './BookForm.css';
+const BookForm = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  // const [formData, setFormData] = useState({}) - если много инпутов
+  const handleSubmit = (e) => {
+    e.preventDefault(); //это чтобы на новую страницу не перенаправлял браузер
+
+    if (title && author) {
+      //dispatch action
+
+      setTitle('');
+      setAuthor(''); // вызов этих двух функции с пустыми строками нужны для того чтобы после ввода данных в инпуты, инпуты очищались и были готовы к новы вводам данных.
+    }
+  };
+  return (
+    <div className="app-block book-form">
+      <h2>Add a New Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+};
+
+export default BookForm;

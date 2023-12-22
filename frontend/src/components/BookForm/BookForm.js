01d67000b89de4b930d06b101d35bc98bb3e5726
Jane Sardarian <sardarianjane@gmail.com>
2023-12-22 18:27:38 +0400

    Separate fn for book creation

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../../redux/books/actionCreators';
+import createBookWithId from '../../utils/createBookWithId';
 import booksData from '../../data/books.json';
 import './BookForm.css';
 const BookForm = () => {
@@ -13,13 +13,7 @@ const BookForm = () => {
   const handleAddRandomBook = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length);
     const randomBook = booksData[randomIndex];
-
-    const randomBookWithId = {
-      ...randomBook,
-      id: uuidv4(),
-      isFavorite: false,
-    };
-    dispatch(addBook(randomBookWithId));
+    dispatch(addBook(createBookWithId(randomBook)));
   };
 
   const handleSubmit = (e) => {
@@ -27,16 +21,8 @@ const BookForm = () => {
 
     if (title && author) {
       //dispatch action
-      const book = {
-        // title: title,
-        // author: author, я могу это сократить, так как название свойства и значения совподают
-        title,
-        author,
-        id: uuidv4(),
-        isFavorite: false,
-      };
 
-      dispatch(addBook(book));
+      dispatch(addBook(createBookWithId({ title, author })));
 
       setTitle('');
       setAuthor(''); // вызов этих двух функции с пустыми строками нужны для того чтобы после ввода данных в инпуты, инпуты очищались и были готовы к новы вводам данных.

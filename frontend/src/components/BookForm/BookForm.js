d986acc55a6a8e73643ec608e1bf2131aae28a1f
Jane Sardarian <sardarianjane@gmail.com>
2023-12-22 13:43:47 +0400

    Initialize Redux, add and display books

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,14 +1,23 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/actionCreators';
 import './BookForm.css';
 const BookForm = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   // const [formData, setFormData] = useState({}) - если много инпутов
+  const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault(); //это чтобы на новую страницу не перенаправлял браузер
 
     if (title && author) {
       //dispatch action
+      const book = {
+        title: title,
+        author: author,
+      };
+
+      dispatch(addBook(book));
 
       setTitle('');
       setAuthor(''); // вызов этих двух функции с пустыми строками нужны для того чтобы после ввода данных в инпуты, инпуты очищались и были готовы к новы вводам данных.
